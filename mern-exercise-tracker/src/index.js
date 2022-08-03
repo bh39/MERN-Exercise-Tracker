139fdabc729edaf5439421d2d91ea2d3a03294d9
bh39 <bh39@rice.edu>
2022-08-03 14:59:50 -0500

    finished project

--- mern-exercise-tracker/src/index.js
+++ mern-exercise-tracker/src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
@@ -10,6 +9,9 @@ root.render(
     <App />
   </React.StrictMode>
 );
+
+// Add this in node_modules/react-dom/index.js
+window.React1 = require('react');
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
